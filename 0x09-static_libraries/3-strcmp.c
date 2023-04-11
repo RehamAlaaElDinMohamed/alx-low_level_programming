3ad35a7b649cfa5b157363aa2fb1fc93b2289c29
rehamalaaeldinmohamed <rehamalaaeldinmohamed@gmail.com>
2023-04-11 10:29:02 -0700

    updated

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,29 +1,23 @@
 #include "main.h"
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
  */
-unsigned int _strspn(char *s, char *accept)
+int _strcmp(char *s1, char *s2)
 {
-	unsigned int n = 0;
-	int r;
+	int i;
 
-
-	while (*s)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		for (r = 0; accept[r]; r++)
+		if (s1[i] != s2[i])
 		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
+			return (s1[i] - s2[i]);
 		}
-		s++;
+	i++;
 	}
-	return (n);
+	return (0);
 }
