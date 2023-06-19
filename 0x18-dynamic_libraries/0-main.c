bca92624a044721375580d4e5e6ab4f9bce31405
rehamalaaeldinmohamed <rehamalaaeldinmohamed@gmail.com>
2023-06-19 05:24:19 -0700

    updated

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
+
--- 0x18-dynamic_libraries/gm/101-make_me_win.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -shared -o /tmp/win.so /root/level-0x18/0x18-dynamic_libraries/101-winning.c
-export LD_PRELOAD=/tmp/win.so
-
